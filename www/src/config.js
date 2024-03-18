6c440b036fd90089815b17c4920d56f74c83c8c7
Leon-Salkovic <leon.salkovic@bayer.com>
2024-03-18 18:39:48 +0100

    Augh

--- www/src/config.js
+++ www/src/config.js
@@ -5,7 +5,7 @@
 const config = {
   "aws_user_pools_web_client_id": "60ni9rgca5i123k7o3nurrkbc",     // CognitoClientID
   "api_base_url": "https://7tcl1uzbd5.execute-api.eu-central-1.amazonaws.com/Stage",                                     // TodoFunctionApi
-  "cognito_hosted_domain": "http://mytodoappdemo-lcslambdareact3.auth.eu-central-1.amazoncognito.com/",                   // CognitoDomainName
+  "cognito_hosted_domain": "mytodoappdemo-lcslambdareact3.auth.eu-central-1.amazoncognito.com/",                   // CognitoDomainName
   "redirect_url": "https://master.d179bjvjhdu2bf.amplifyapp.com/"                                      // AmplifyURL
 };
 
