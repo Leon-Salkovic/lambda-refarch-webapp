1a3acde45e0e86f9ce3d898354ae103463e37c02
Leon-Salkovic <leon.salkovic@bayer.com>
2024-03-18 17:45:27 +0100

    Apdate

--- www/src/config.js
+++ www/src/config.js
@@ -1,6 +1,7 @@
 // You can obtain these values by running:
 // aws cloudformation describe-stacks --stack-name <YOUR STACK NAME> --query "Stacks[0].Outputs[]"
 
+
 const config = {
   "aws_user_pools_web_client_id": "60ni9rgca5i123k7o3nurrkbc",     // CognitoClientID
   "api_base_url": "https://7tcl1uzbd5.execute-api.eu-central-1.amazonaws.com/Stage",                                     // TodoFunctionApi
